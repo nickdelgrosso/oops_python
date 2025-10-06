5b39c346f25ad6392f86d53c633789dc53d7a954
Nicholas A. Del Grosso <delgrosso.nick@gmail.com>
2025-10-06 15:17:29 +0200

    new feature!

--- main.py
+++ main.py
@@ -1,6 +1,17 @@
+import os
+import json
+import math
 def main():
     print("Hello from oops-python!")
     print("1 + 1 = 2")
+
+
+a=     3
+b    = 5
+c = sum([a,b  + 1])    
+
+rsult = c
+print(c)
 
 
 if __name__ == "__main__":
